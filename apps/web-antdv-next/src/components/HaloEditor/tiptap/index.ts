8f85d09f32cd16f3219cc91b81e697332dd73a49
chuxijin <1939665486@qq.com>
2026-06-18 08:04:09 +0800

    chore: clean up unused/non-existent prosemirror exports to fix vite build

--- apps/web-antdv-next/src/components/HaloEditor/tiptap/index.ts
+++ apps/web-antdv-next/src/components/HaloEditor/tiptap/index.ts
@@ -2,22 +2,7 @@ export { Editor, type MarkType, MarkView, type NodeType } from './core';
 export * from './pm';
 export {
   Fragment,
-  liftListItem,
-  type Command as PMCommand,
-  InputRule as PMInputRule,
-  Mark as PMMark,
-  type MarkType as PMMarkType,
-  type MarkView as PMMarkView,
   Node as PMNode,
-  type NodeRange as PMNodeRange,
-  type NodeType as PMNodeType,
-  type NodeView as PMNodeView,
-  textblockTypeInputRule as pmTextblockTypeInputRule,
-  wrappingInputRule as pmWrappingInputRule,
-  sinkListItem,
-  splitListItem,
-  undoInputRule,
-  wrapInList,
 } from './pm';
 export * from './vue-3';
 export {
